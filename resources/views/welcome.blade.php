9e8129b0cbd3802488d701b8df08d7d103a7bc27
nasser <nasservb@gmail.com>
2021-11-29 06:30:50 +0330

    vuejs version

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Laravel</title>
+
+    <link rel="stylesheet" href="/css/app.css" />
+</head>
+<body>
+<div id="app"></div>
+<script src="/js/app.js"></script>
+</body>
+</html>
